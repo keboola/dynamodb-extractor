17d74dc2c6750251e2c822db3987dc1b06714555
Vladimír Kriška <vlado@keboola.com>
2017-02-06 17:30:19 +0100

    Simplify FileHelper

--- src/Keboola/DynamoDbExtractor/FileHelper.php
+++ src/Keboola/DynamoDbExtractor/FileHelper.php
@@ -8,14 +8,12 @@ class FileHelper
      * Appends content to file
      * @param string $filename
      * @param string $content
-     * @throws Exception
+     * @throws \Exception
      */
     public static function appendContentToFile(string $filename, string $content)
     {
-        if (@file_put_contents($filename, $content, FILE_APPEND | LOCK_EX) === false) {
-            $message = 'Failed write to file "' . $filename . '". Free space '
-                . disk_free_space(dirname($filename)) . '.';
-            throw new Exception($message, 0, null);
+        if (file_put_contents($filename, $content, FILE_APPEND | LOCK_EX) === false) {
+            throw new \Exception('Failed write to file: ' . $filename);
         }
     }
 }
