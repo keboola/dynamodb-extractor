89dc504dfa107484feb0b3ea380f8e143a269717
Vladimír Kriška <vlado@keboola.com>
2017-02-07 00:26:40 +0100

    Convert all errors to exceptions

---
+++ src/common.php
@@ -0,0 +1,8 @@
+<?php
+
+set_error_handler(function ($severity, $message) {
+    if (!(error_reporting() & $severity)) {
+        return false;
+    }
+    throw new \ErrorException($message);
+});
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/common.php';
 require __DIR__ . '/Keboola/DynamoDbExtractor/ExtractorTestCase.php';
