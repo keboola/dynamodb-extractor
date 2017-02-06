8cd206c6399cbfba0d128e6048b52f93b971eea7
Vladimír Kriška <vlado@keboola.com>
2017-02-06 22:39:36 +0100

    Test also exceptions in ConfigFileNotFoundTest

--- tests/Keboola/DynamoDbExtractor/ConfigFileNotFoundTest.php
+++ tests/Keboola/DynamoDbExtractor/ConfigFileNotFoundTest.php
@@ -10,7 +10,7 @@ class ConfigFileNotFoundTest extends ExtractorTestCase
     /** @var string */
     protected $dataDir = '/tmp/config-file-not-found';
 
-    public function testRun()
+    public function testConfigFileNotFound()
     {
         $application = new Application;
         $application->add(new RunCommand);
@@ -24,9 +24,23 @@ class ConfigFileNotFoundTest extends ExtractorTestCase
         ]);
 
         $this->assertSame(2, $exitCode);
-        $this->assertContains(
-            'Application error',
-            $commandTester->getDisplay()
-        );
+    }
+
+    public function testConfigFileNotFoundTestMode()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Config file not found');
+
+        $application = new Application;
+        $application->add(new RunCommand);
+
+        $command = $application->find('run');
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'command' => $command->getName(),
+            'data directory' => $this->dataDir,
+            '--test-mode' => true,
+        ]);
     }
 }
