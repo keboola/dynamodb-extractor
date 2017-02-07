68931ad2a312b383870bc37f9fae6864090554bf
Vladimír Kriška <vlado@keboola.com>
2017-02-07 09:51:40 +0100

    Test duplicate export names

--- src/Keboola/DynamoDbExtractor/Extractor.php
+++ src/Keboola/DynamoDbExtractor/Extractor.php
@@ -24,7 +24,7 @@ class Extractor
 
         // check for duplicate export names
         if (count($this->parameters['exports']) !== count(array_unique(array_column($this->parameters['exports'], 'name')))) {
-            throw new \Exception('Please remove duplicate export names');
+            throw new UserException('Please remove duplicate export names');
         }
 
         // create DynamoDbClient instance
