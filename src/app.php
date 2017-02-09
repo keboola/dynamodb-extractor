6d109ff6b6b7f76ee4fae4d3db45e3481f7181d5
Vladimír Kriška <vlado@keboola.com>
2017-02-09 22:35:40 +0100

    Use ConsoleOutput explicitly in app

--- src/app.php
+++ src/app.php
@@ -5,7 +5,8 @@ require __DIR__ . '/common.php';
 
 use Symfony\Component\Console\Application;
 use Keboola\DynamoDbExtractor\RunCommand;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 $application = new Application;
 $application->add(new RunCommand);
-$application->run();
+$application->run(null, new ConsoleOutput());
